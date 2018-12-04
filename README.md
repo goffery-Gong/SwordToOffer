6e971969fddb083acb1e14ae463b1c10f0934b52
goffery-Gong <petro_gong@163.com>
2018-12-04 20:27:39 +0800

    斐波那契2

--- README.md
+++ README.md
@@ -110,3 +110,19 @@ n=4，看看程序怎么跑的：
             return f;
         }
     }; 
+    
+### 8 变态跳台阶
+因为n级台阶，第一步有n种跳法：跳1级、跳2级、到跳n级  
+跳1级，剩下n-1级，则剩下跳法是f(n-1)  
+跳2级，剩下n-2级，则剩下跳法是f(n-2)  
+以此类推，到跳n级，剩下n-n=0；剩下跳法f(0)=0  
+所以f(n)=f(n-1)+f(n-2)+...+f(1)+f(0)  
+因为f(n-1)=f(n-2)+f(n-3)+...+f(1)+f(0)  
+所以f(n)=2*f(n-1)递推出，f(n)=2^(n-1)
+
+    public int JumpFloorII(int target) {
+            if(target<=0) return 0;
+            if(target==1) return 1;
+            else 
+                return 2*JumpFloorII(target-1);
+    }
--- src/part1/Fibonacci.java
+++ src/part1/Fibonacci.java
@@ -1,7 +1,13 @@
 package part1;
 
 public class Fibonacci {
-    public int FibonacciMethod(int n) {
+
+	/**
+	 * 循环 O(n)
+	 * @param n
+	 * @return
+	 */
+    public int FibonacciCycleMethod(int n) {
 		int a = 1, b = 1;
 		int sum = 0;
 		if (n == 0)
@@ -16,5 +22,19 @@ public class Fibonacci {
 			}
 			return sum;
 		}
+	}
+
+	/**
+	 * 递归 O(2^n)(https://www.nowcoder.com/questionTerminal/fd57dad14d224881a929d6739741fe50)
+	 * @param n
+	 * @return
+	 */
+	public static int FibonaciDiguiMethod(int n){
+		if(n <= 0)
+			return 0;
+		if(n == 1)
+			return 1;
+		else
+			return FibonaciDiguiMethod(n-1)+FibonaciDiguiMethod(n-2);
 	}
 }
